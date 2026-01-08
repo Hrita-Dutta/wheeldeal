d40c2324c873f31150f3ec9e8637287fd9995434
Hrita Dutta <hritadutta@gmail.com>
2026-01-08 15:25:40 +0545

    made footer mobile responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import logo from "../../public/logo/logo_alt.png";
 import { NavLink } from "react-router-dom";
+import { scrollToTop } from "../utils/ScrollToTop";
 
 const Footer = () => {
   return (
     <>
-      <footer className="flex justify-center py-1">
-        <div className="w-295">
-          <div className="flex justify-between py-10">
-            <div className="w-[23%] flex flex-col gap-3">
+      <footer className="flex justify-center py-1 px-4">
+        <div className="w-full lg:w-295">
+          <div className="flex flex-col lg:flex-row justify-between py-10 gap-8 lg:gap-0">
+            <div className="w-full lg:w-[23%] flex flex-col gap-3">
               <img src={logo} alt="" width={200} />
               <p>
                 Your trusted vehicle rental platform Rent cars, bikes, and more
@@ -16,22 +17,26 @@ const Footer = () => {
               </p>
               <span>social icons</span>
             </div>
-            <div className="text-[#64748B] ">
+            <div className="text-[#64748B]">
               <ul className="flex flex-col gap-2">
                 <li className="font-bold italic text-[#1E3A5F]">Quick Links</li>
                 <li className="hover:underline">
-                  <NavLink>Home</NavLink>
+                  <NavLink to="/" onClick={scrollToTop}>
+                    Home
+                  </NavLink>
                 </li>
                 <li className="hover:underline">
-                  <NavLink>Vehicles</NavLink>
+                  <NavLink onClick={scrollToTop}>Vehicles</NavLink>
                 </li>
                 <li className="hover:underline">
-                  <NavLink>About Us</NavLink>
+                  <NavLink to="/about" onClick={scrollToTop}>
+                    About Us
+                  </NavLink>
                 </li>
               </ul>
             </div>
             {/* For Customers or Owners according to account type */}
-            <div className="text-[#64748B] ">
+            <div className="text-[#64748B]">
               <ul className="flex flex-col gap-2">
                 <li className="font-bold italic text-[#1E3A5F]">
                   For Customers/Owners
@@ -50,7 +55,7 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div className="text-[#64748B] ">
+            <div className="text-[#64748B]">
               <ul className="flex flex-col gap-2">
                 <li className="font-bold italic text-[#1E3A5F]">Support</li>
                 <li className="hover:underline">
